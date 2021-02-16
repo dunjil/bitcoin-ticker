35ee4d8aa58868e11c150144c83a015520b17ea4
dunjil <jsduna@gmail.com>
2021-02-16 20:24:11 +0100

    Simple Bitcoin Price Ticker

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -27,5 +30,20 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
+const bitcoinAverageURL =
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
 
-class CoinData {}
+class CoinData {
+  Future getCoinData(String pair) async {
+    String requestURL = '$bitcoinAverageURL/$pair';
+    http.Response response = await http.get(requestURL);
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['last'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/services.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,45 +10,167 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedItem = 'USD';
+  String bitcoinValue = '';
+  String ethValue = '';
+  String ltcValue = '';
+  String bitcoinPair = '';
+  String ethPair = '';
+  String ltcPair = '';
+  @override
+  void initState() {
+    super.initState();
+    updateUI();
+  }
+
+  updateUI() {
+    bitcoinPair = 'BTC$selectedItem';
+    ethPair = 'ETH$selectedItem';
+    ltcPair = 'LTC$selectedItem';
+    getBitcoinData();
+    getEthData();
+    getLtcData();
+  }
+
+  void getBitcoinData() async {
+    try {
+      double data = await CoinData().getCoinData(bitcoinPair);
+      setState(() {
+        bitcoinValue = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getEthData() async {
+    try {
+      double data = await CoinData().getCoinData(ethPair);
+      setState(() {
+        ethValue = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getLtcData() async {
+    try {
+      double data = await CoinData().getCoinData(ltcPair);
+      setState(() {
+        ltcValue = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  DropdownButton<String> getDropdownButton() {
+    List<DropdownMenuItem<String>> listItems = [];
+    for (String item in currenciesList) {
+      var newItem = DropdownMenuItem(
+        value: item,
+        child: Text(item),
+      );
+      listItems.add(newItem);
+    }
+    return DropdownButton<String>(
+        icon: Icon(Icons.attach_money),
+        value: selectedItem,
+        items: listItems,
+        onChanged: (value) {
+          setState(() {
+            selectedItem = value;
+            updateUI();
+          });
+        });
+  }
+
+  CupertinoPicker getCupertinoPicker() {
+    List<Text> pickerItems = [];
+    for (String item in currenciesList) {
+      pickerItems.add(Text(
+        item,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+    }
+    return CupertinoPicker(
+      magnification: 1.0,
+      useMagnifier: true,
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 30.0,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedItem = currenciesList[selectedIndex];
+          updateUI();
+        });
+      },
+      children: pickerItems,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        leading: Icon(Icons.attach_money),
+        title: Text(' Coin Ticker'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          PriceCard(
+              coinValue: bitcoinValue,
+              coinType: 'BTC',
+              selectedItem: selectedItem),
+          PriceCard(
+              coinValue: ethValue, coinType: 'ETH', selectedItem: selectedItem),
+          PriceCard(
+              coinValue: ltcValue, coinType: 'LTC', selectedItem: selectedItem),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? getCupertinoPicker() : getDropdownButton(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class PriceCard extends StatelessWidget {
+  const PriceCard({this.coinValue, this.coinType, this.selectedItem});
+
+  final String coinValue;
+  final String coinType;
+  final String selectedItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $coinType = $coinValue   $selectedItem',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
